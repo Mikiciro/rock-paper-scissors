298991dc5edc9e92077faa002fd1200393d4f8e8
Mikiciro <cirollamiki@gmail.com>
2024-01-10 12:54:38 +0100

    creata la prima funzione

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello world!')
+const choises = ['Rock', 'Paper', 'Scissors',];
+
+function getComputerChoise() {
+  return choises[Math.floor(Math.random() * choises.length)];
+}
+
+function playRound(playerSelection, computerSelection) {
+        
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoise();
+console.log(playRound(playerSelection, computerSelection));
