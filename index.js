fd662060ae4e2369b4de012126c26a5939edad9a
Mikiciro <cirollamiki@gmail.com>
2024-01-10 13:11:19 +0100

    implementata funzione base unico round

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-const choises = ['Rock', 'Paper', 'Scissors',];
+const choises = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoise() {
   return choises[Math.floor(Math.random() * choises.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
-        
+    if (playerSelection == "Rock" && computerSelection == "Scissors") {
+        return "You win! Rock beat Scissors";
+    } else if (playerSelection == "Rock" && computerSelection == "Paper"){
+        return "You lose! Paper beat Rock";
+    } else {
+        return "Draw!";
+    }
 }
 
-const playerSelection = 'rock';
+const playerSelection = "Rock";
 const computerSelection = getComputerChoise();
+console.log("His choise is: " + computerSelection);
+console.log("My choise is: " + playerSelection);
 console.log(playRound(playerSelection, computerSelection));
