cdd8153b4cda01f4746410d64d65dd464572c8e7
Mikiciro <cirollamiki@gmail.com>
2024-02-15 10:29:32 +0100

    ripristinato codice di partenza

--- script.js
+++ script.js
@@ -4,37 +4,40 @@ function getComputerChoice() {
   return choises[Math.floor(Math.random() * choises.length)];
 }
 
-//function game() {
-  // var player = 0;
-  // var computer = 0;
-  // var round = 3;
-  // for (let i = 0; i < round; i++) {
-  //   var playerChoice = document.getElementById("playerChoice").value;
-  //   var computerChoice = getComputerChoice();
-  //   document.getElementById("computerChoice").value = computerChoice;
-  //   var result = playRound(playerChoice, computerChoice);
-  //   if (result.includes("win")) {
-  //     player++;
-  //   } else if (result.includes("lose")) {
-  //     computer++;
-  //   }
-  //   document.getElementById("gameResult").innerHTML = result + "<br>";
-  // }
-  // if (player > computer) {
-  //   document.getElementById("gameResult").innerHTML += " Greatings! You win!";
-  // } else if (player == computer) {
-  //   document.getElementById("gameResult").innerHTML += " It's a Draw! Another game?";
-  // } else {
-  //   document.getElementById("gameResult").innerHTML += " Sorry! You lose!";
-  // }
-//}
+function game() {
+  var player = 0;
+  var computer = 0;
+  var round = 1;
 
-const rock = document.querySelector("#rock");
-rock.addEventListener('click', playRound);
-const paper = document.querySelector("#paper");
-paper.addEventListener('click', playRound);
-const scissors = document.querySelector("#scissors");
-scissors.addEventListener('click', playRound);
+  for (let i = 0; i < round; i++) {
+    var playerChoice = document.getElementById("playerChoice").value;
+    var computerChoice = getComputerChoice();
+    document.getElementById("computerChoice").value = computerChoice;
+    var result = playRound(playerChoice, computerChoice);
+
+    if (result.includes("win")) {
+      player++;
+    } else if (result.includes("lose")) {
+      computer++;
+    }
+    document.getElementById("gameResult").innerHTML = result + "<br>";
+  }
+
+  if (player > computer) {
+    document.getElementById("gameResult").innerHTML += " Greatings! You win!";
+  } else if (player == computer) {
+    document.getElementById("gameResult").innerHTML += " It's a Draw! Another game?";
+  } else {
+    document.getElementById("gameResult").innerHTML += " Sorry! You lose!";
+  }
+}
+
+// const rock = document.querySelector("#rock");
+// rock.addEventListener('click', playRound);
+// const paper = document.querySelector("#paper");
+// paper.addEventListener('click', playRound);
+// const scissors = document.querySelector("#scissors");
+// scissors.addEventListener('click', playRound);
 
 function playRound(playerSelection, computerSelection) {
   var winAgainst = {
@@ -43,88 +46,75 @@ function playRound(playerSelection, computerSelection) {
     rock: "scissors",
   };
 
-  if (playerSelection, computerSelection) {
-    console.log("draw");
-  }
-
-  if (winAgainst[playerSelection], computerSelection) {
-    return console.log("you win");
-  } else {
-    return console.log("you lose");
-  }
+  if (equals(playerSelection, computerSelection)) {
+     return "Draw";
+   }
+   if (equals(winAgainst[playerSelection], computerSelection)) {
+     return "You win! " + playerSelection + " beat " + computerSelection;
+   } else {
+     return "You lose! " + computerSelection + " beat " + playerSelection;
+   }
 }
-
-
-
-//   if (equals(playerSelection, computerSelection)) {
-//     return "Draw";
-//   }
-//   if (equals(winAgainst[playerSelection], computerSelection)) {
-//     return "You win! " + playerSelection + " beat " + computerSelection;
-//   } else {
-//     return "You lose! " + computerSelection + " beat " + playerSelection;
-//   }
-
-// function equals(a, b) {
-//   return a.toLowerCase() == b.toLowerCase();
-// }
+ function equals(a, b) {
+   return a.toLowerCase() == b.toLowerCase();
+ }
 
 //FUNZIONE DEI TEST
 
-// function testPlayRound() {
-//   if (playRound("paper", "scissors") == "You lose! scissors beat paper") {
-//     console.log("test ok");
-//   } else {
-//     console.log("test ko");
-//   }
+function testPlayRound() {
+  if (playRound("paper", "scissors") == "You lose! scissors beat paper") {
+    console.log("test ok");
+  } else {
+    console.log("test ko");
+  }
 
-//   if (playRound("paper", "rock") == "You win! paper beat rock") {
-//     console.log("test ok");
-//   } else {
-//     console.log("test ko");
-//   }
+  if (playRound("paper", "rock") == "You win! paper beat rock") {
+    console.log("test ok");
+  } else {
+    console.log("test ko");
+  }
 
-//   if (playRound("paper", "paper") == "Draw") {
-//     console.log("test ok");
-//   } else {
-//     console.log("test ko");
-//   }
+  if (playRound("paper", "paper") == "Draw") {
+    console.log("test ok");
+  } else {
+    console.log("test ko");
+  }
 
-//   if (playRound("rock", "paper") == "You lose! paper beat rock") {
-//     console.log("test ok");
-//   } else {
-//     console.log("test ko");
-//   }
+  if (playRound("rock", "paper") == "You lose! paper beat rock") {
+    console.log("test ok");
+  } else {
+    console.log("test ko");
+  }
 
-//   if (playRound("rock", "scissors") == "You win! rock beat scissors") {
-//     console.log("test ok");
-//   } else {
-//     console.log("test ko");
-//   }
+  if (playRound("rock", "scissors") == "You win! rock beat scissors") {
+    console.log("test ok");
+  } else {
+    console.log("test ko");
+  }
 
-//   if (playRound("rock", "rock") == "Draw") {
-//     console.log("test ok");
-//   } else {
-//     console.log("test ko");
-//   }
+  if (playRound("rock", "rock") == "Draw") {
+    console.log("test ok");
+  } else {
+    console.log("test ko");
+  }
 
-//   if (playRound("scissors", "rock") == "You lose! rock beat scissors") {
-//     console.log("test ok");
-//   } else {
-//     console.log("test ko");
-//   }
+  if (playRound("scissors", "rock") == "You lose! rock beat scissors") {
+    console.log("test ok");
+  } else {
+    console.log("test ko");
+  }
 
-//   if (playRound("scissors", "paper") == "You win! scissors beat paper") {
-//     console.log("test ok");
-//   } else {
-//     console.log("test ko");
-//   }
+  if (playRound("scissors", "paper") == "You win! scissors beat paper") {
+    console.log("test ok");
+  } else {
+    console.log("test ko");
+  }
 
-//   if (playRound("scissors", "scissors") == "Draw") {
-//     console.log("test ok");
-//   } else {
-//     console.log("test ko");
-//   }
-// }
+  if (playRound("scissors", "scissors") == "Draw") {
+    console.log("test ok");
+  } else {
+    console.log("test ko");
+  }
+}
 
-// testPlayRound();
+testPlayRound();
